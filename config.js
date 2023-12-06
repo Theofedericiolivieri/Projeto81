e99971e5d71f6adb0c4efa28f90c7f0d143f6a88
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2023-12-06 20:20:22 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCg8-TKhUm_8nnavkqDqPBGFZqAnSDW-QY",
+    authDomain: "projeto81-16b00.firebaseapp.com",
+    databaseURL: "https://projeto81-16b00-default-rtdb.firebaseio.com",
+    projectId: "projeto81-16b00",
+    storageBucket: "projeto81-16b00.appspot.com",
+    messagingSenderId: "676842010780",
+    appId: "1:676842010780:web:ec9db940cfc975e0ecd559"
+  };

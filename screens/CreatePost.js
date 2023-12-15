7135dea42e164e452317a38158e008315cd6cd9d
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2023-12-15 19:57:12 -0300

    Add files via upload

--- screens/CreatePost.js
+++ screens/CreatePost.js
@@ -107,6 +107,13 @@ return(
                 })
                 }
                 />
+                 <View style={styles.submitButton}>
+                <Button
+                  onPress={() => this.addPost()}
+                  title="Submit"
+                  color="#841584"
+                />
+              </View>
             </View>
             <TextInput
             style={styles.inputFont}
@@ -119,3 +126,69 @@ return(
        <View style={{flex: 0.08}} />
     </View>
 );
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#15193c",
+    },
+    droidSafeArea: {
+      marginTop:
+        Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+    },
+    appTitle: {
+      flex: 0.07,
+      flexDirection: "row",
+    },
+    appIcon: {
+      flex: 0.3,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    iconImage: {
+      width: "100%",
+      height: "100%",
+      resizeMode: "contain",
+    },
+    appTitleTextContainer: {
+      flex: 0.7,
+      justifyContent: "center",
+    },
+    appTitleText: {
+      color: "white",
+      fontSize: RFValue(28),
+      fontFamily: "Bubblegum-Sans",
+    },
+    fieldsContainer: {
+      flex: 0.85,
+    },
+    previewImage: {
+      width: "93%",
+      height: RFValue(250),
+      alignSelf: "center",
+      borderRadius: RFValue(10),
+      marginVertical: RFValue(10),
+      resizeMode: "contain",
+    },
+    inputFont: {
+      height: RFValue(40),
+      marginTop: RFValue(40),
+      borderColor: "white",
+      borderWidth: RFValue(1),
+      borderRadius: RFValue(10),
+      paddingLeft: RFValue(10),
+      color: "white",
+      fontFamily: "Bubblegum-Sans",
+    },
+    inputFontExtra: {
+      marginTop: RFValue(15),
+    },
+    inputTextBig: {
+      textAlignVertical: "top",
+      padding: RFValue(5),
+    },
+    submitButton: {
+      marginTop: RFValue(20),
+      alignItems: "center",
+      justifyContent: "center"
+    }
+  });
